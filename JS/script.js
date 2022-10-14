c7b0671dc9d6bbce843d155a90c86647d4c005d9
Anan  Mohsen <99495940+Anan45Mohsen@users.noreply.github.com>
2022-10-14 15:15:06 +0200

    create project

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+
+document.getElementById("btn1").onclick = function(){
+    document.getElementById("p1").innerHTML = "100$"
+}
+
+document.getElementById("btn2").onclick = function(){
+    document.getElementById("p2").innerHTML = "200$"
+}
+document.getElementById("btn3").onclick = function(){
+    document.getElementById("p3").innerHTML = "300$"
+}
+document.getElementById("btn4").onclick = function(){
+    document.getElementById("p4").innerHTML = "400$"
+}
+document.getElementById("btn5").onclick = function(){
+    document.getElementById("p5").innerHTML = "500$"
+}
+document.getElementById("btn6").onclick = function(){
+    document.getElementById("p6").innerHTML = "600$"
+}
